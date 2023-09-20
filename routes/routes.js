31a284c76e83a9406c5e79d403ed39b1565ed324
DeibyAlexander <deiby2707@gmail.com>
2023-09-20 09:26:40 -0500

    feat: :sparkles: Falta en insert del endPoint #11

--- routes/routes.js
+++ routes/routes.js
@@ -55,7 +55,7 @@ router.get("/endpoint1", async (req,res)=>{
 })
 
 
-//! Obtener las citas de una fecha en específico , donde se ordene los pacientes de manera alfabética.
+//? Obtener las citas de una fecha en específico , donde se ordene los pacientes de manera alfabética.
 
 router.get("/endpoint2", async (req,res)=>{
     
@@ -104,7 +104,7 @@ router.get("/endpoint2", async (req,res)=>{
 })
 
 
-//! Obtener todos los médicos de una especialidad en específico (por ejemplo, ‘Cardiología’).
+//? Obtener todos los médicos de una especialidad en específico (por ejemplo, ‘Cardiología’).
 
 router.get("/endpoint3", async (req,res)=>{
     
@@ -155,7 +155,7 @@ router.get("/endpoint3", async (req,res)=>{
 })
 
 
-// !Encontrar la próxima cita para un paciente en específico (por ejemplo, el paciente con user_id 1).
+//? Encontrar la próxima cita para un paciente en específico (por ejemplo, el paciente con user_id 1).
 
 router.get("/endpoint4", async (req,res)=>{
     const client = new MongoClient(conexiondb)
@@ -208,8 +208,7 @@ router.get("/endpoint4", async (req,res)=>{
 })
 
 
-//! Obtener todos los médicos de una especialidad en específico (por ejemplo, ‘Cardiología’).
-
+//? Encontrar todos los pacientes que tienen citas con un médico en específico (por ejemplo, el médico con med_numMatriculaProfesional 1).
 
 router.get("/endpoint5", async (req, res)=>{
 
@@ -295,7 +294,7 @@ router.get("/endpoint5", async (req, res)=>{
 })
 
 
-//! Obtener todos los médicos de una especialidad en específico (por ejemplo, ‘Cardiología’).
+//? Encontrar todas las citas de un día en específico (por ejemplo, ‘2023-07-12’)
 
 router.get("/endpoint6", async (req,res)=>{ 
 
@@ -332,7 +331,7 @@ router.get("/endpoint6", async (req,res)=>{
     }
 })
 
-//! Obtener todos los médicos con sus consultorios correspondientes.
+//? Obtener todos los médicos con sus consultorios correspondientes.
 
 router.get("/endpoint7", async (req,res)=>{
     
@@ -383,7 +382,7 @@ router.get("/endpoint7", async (req,res)=>{
 })
 
 
-//! Contar el número de citas que un médico tiene en un día específico (por ejemplo, el médico con med_numMatriculaProfesional 1 en ‘2023-07-12’).
+//? Contar el número de citas que un médico tiene en un día específico (por ejemplo, el médico con med_numMatriculaProfesional 1 en ‘2023-07-12’).
 
 router.get("/endpoint8", async (req,res)=>{
     
@@ -396,30 +395,10 @@ router.get("/endpoint8", async (req,res)=>{
         const db = client.db(nombredb)
         const collection = db.collection("Usuarios")
         const result = await collection.aggregate([
-            
-                {
-                    $lookup:{
-                        from: "Acudiente",
-                        localField :"usu_acudente",
-                        foreignField: "acu_codigo",
-                        as: "usu_acudente"
-                    }
-                },
-                {
-                    $unwind: "$usu_acudente"
-                },
-                {
-                    $match: {
-                        "usu_acudente.acu_nombreCompleto":"Jose ANtonio Alvarez"  
-                    }
-                },
-                {
-                    $project:{
-                        _id:0,
-                        "usu_acudente.acu_nombreCompleto": 1,
-                    }
-                }
-            ]).toArray()
+            {
+
+            }
+        ]).toArray()
 
         res.json(result)
    
@@ -435,7 +414,7 @@ router.get("/endpoint8", async (req,res)=>{
 })
 
 
-//! Obtener lo/s consultorio/s donde se aplicó las citas de un paciente.
+//? Obtener lo/s consultorio/s donde se aplicó las citas de un paciente.
 
 router.get("/endpoint9", async (req, res) => {
     const client = new MongoClient(conexiondb);
@@ -511,7 +490,7 @@ router.get("/endpoint9", async (req, res) => {
 })
 
 
-//! Obtener todas las citas realizadas por los pacientes de acuerdo al género registrado, siempre y cuando el estado de la cita se encuentra registrada como “Atendida”.
+//? Obtener todas las citas realizadas por los pacientes de acuerdo al género registrado, siempre y cuando el estado de la cita se encuentra registrada como “Atendida”.
 
 router.get("/endpoint10/:genero", async (req,res)=>{
     
@@ -611,7 +590,7 @@ router.get("/endpoint10/:genero", async (req,res)=>{
 })
 
 
-//! Insertar un paciente a la tabla usuario, donde si es menor de edad deberá solicitar primero que ingrese el acudiente y validar si ya estaba registrado el acudiente (El usuario deberá poder ingresar de manera personalizada los datos del usuario a ingresar).
+//? Insertar un paciente a la tabla usuario, donde si es menor de edad deberá solicitar primero que ingrese el acudiente y validar si ya estaba registrado el acudiente (El usuario deberá poder ingresar de manera personalizada los datos del usuario a ingresar).
 
 
 router.get("/endpoint11/:edades/", async (req, res) => {
@@ -627,15 +606,15 @@ router.get("/endpoint11/:edades/", async (req, res) => {
 
         const edad = req.params.edades
 
-        // Verificar si el paciente es menor de edad
+ 
         if (edad < 18) {
             
-            // Si es menor de edad, se requiere un acudiente
+
             if (!usu_acudente) {
                 return res.status(400).json({ message: 'Se requiere un acudiente para pacientes menores de edad' });
             }
-
-            // Verificar si el acudiente existe en la base de datos
+ 
+            // * Flata validar si existe o no el acudente ingresado
       
 
             const acudiente = await db.collection("Usuarios").aggregate( [
@@ -698,6 +677,101 @@ router.get("/endpoint11/:edades/", async (req, res) => {
     } finally {
         client.close();
         console.log('Servidor Cerrado');
+    }
+})
+
+
+//? Mostrar todas las citas que fueron canceladas de un mes en específico. Dicha consulta deberá mostrar la fecha de la cita, el nombre del usuario y el médico designado.
+
+router.get("/endpoint12/:mes", async (req,res)=>{
+    
+    const client = new MongoClient(conexiondb)
+
+    const mesEspecifico = req.params.mes
+
+
+    try {
+        
+        await client.connect()
+        const db = client.db(nombredb)
+        const collection = db.collection("Cita")
+        const result = await collection.aggregate([
+            {
+                $match: {
+                    cit_fecha: {$gte: new Date(2023, mesEspecifico - 1, 1),
+                    $lt: new Date(2023, mesEspecifico, 1)}
+                }
+            },
+            {
+                $lookup:{
+                    from: "Estado_Cita",
+                    localField: "cit_estadoCita",
+                    foreignField: "estcita_id",
+                    as: "cit_estadoCita"
+                }
+            },
+            {
+                $unwind: "$cit_estadoCita"
+            },
+            {
+                $match: {
+                    "cit_estadoCita.estcita_nombre" : "Cancelada"
+                }
+            },
+            {
+                $lookup:{
+                    from: "Usuarios",
+                    localField: "cit_datosUsuario",
+                    foreignField: "usu_id",
+                    as: "cit_datosUsuario"
+                }
+            },
+            {
+                $unwind: "$cit_datosUsuario"
+            },
+            {
+                $lookup:{
+                    from: "Medico",
+                    localField: "cit_medico",
+                    foreignField: "med_nroMatriculo",
+                    as: "cit_medico"
+                }
+            },
+            {
+                $unwind: "$cit_medico"
+            },
+            {
+                $project:{
+                    _id: 0,
+                    cit_codigo: 1,
+                    cit_fecha: 1,
+ 
+                    "cit_estadoCita.estcita_id": 1,
+                    "cit_estadoCita.estcita_nombre": 1,
+
+                    "cit_medico.med_nombre_Completo": 1,
+                    "cit_medico.med_nroMatriculo": 1,
+    
+                    "cit_datosUsuario.usu_id": 1,
+                    "cit_datosUsuario.usu_nombre": 1,
+                    "cit_datosUsuario.usu_segundo_nombre": 1,
+                    "cit_datosUsuario.usu_primer_apellido_usuar": 1,
+                    "cit_datosUsuario.usu_segdo_apellido_usuar": 1,
+
+                }
+            }
+        ]).toArray()
+
+        res.json(result)
+   
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }finally{
+
+        client.close()
+        console.log("Servidor cerrado")
     }
 })
 
